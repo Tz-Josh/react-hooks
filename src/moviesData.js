c05002a5dd3eaac53a5d2fed8072d7eff5ab4290
Joshua Edumoh <liljosh196@gmail.com>
2026-08-06 19:31:53 +0100

    Create moviesData.js

---
+++ src/moviesData.js
@@ -0,0 +1,46 @@
+const moviesData = [
+  {
+    id: 1,
+    title: "Inception",
+    description: "A thief who steals corporate secrets through the use of dream-sharing technology is given the inverse task of planting an idea into the mind of a C.E.O.",
+    posterURL: "https://image.tmdb.org/t/p/w500/9gk7adHYeDvHkCSEqAvQNLV5Uge.jpg",
+    rating: 4.8
+  },
+  {
+    id: 2,
+    title: "The Shawshank Redemption",
+    description: "Two imprisoned men bond over a number of years, finding solace and eventual redemption through acts of common decency.",
+    posterURL: "https://image.tmdb.org/t/p/w500/q6y0Go1tsGEsmtFryDOJo3dEmqu.jpg",
+    rating: 4.9
+  },
+  {
+    id: 3,
+    title: "The Dark Knight",
+    description: "When the menace known as the Joker wreaks havoc and chaos on the people of Gotham, Batman must accept one of the greatest psychological and physical tests of his ability to fight injustice.",
+    posterURL: "https://image.tmdb.org/t/p/w500/qJ2tW6WMUDux911r6m7haRef0WH.jpg",
+    rating: 4.7
+  },
+  {
+    id: 4,
+    title: "Pulp Fiction",
+    description: "The lives of two mob hitmen, a boxer, a gangster and his wife intertwine in four tales of violence and redemption.",
+    posterURL: "https://image.tmdb.org/t/p/w500/d5iIlFn5s0ImszYzBPb8vIfUBHJ.jpg",
+    rating: 4.6
+  },
+  {
+    id: 5,
+    title: "Forrest Gump",
+    description: "The presidencies of Kennedy and Johnson, the events of Vietnam, Watergate, and other historical events unfold through the perspective of an Alabama man with an IQ of 75.",
+    posterURL: "https://image.tmdb.org/t/p/w500/arw2vcBveWOVZr6pxd9XTd1TdQa.jpg",
+    rating: 4.5
+  },
+  {
+    id: 6,
+    title: "The Matrix",
+    description: "A computer hacker learns from mysterious rebels about the true nature of his reality and his role in the war against its controllers.",
+    posterURL: "https://image.tmdb.org/t/p/w500/f89U3ADr1oiB1s9GkdPOEpXUk5H.jpg",
+    rating: 4.4
+  }
+];
+
+export default moviesData;
